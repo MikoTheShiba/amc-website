eb8ee2a3906a395e2dd8583afd23a69a6b8458c4
MikoTheShiba <mikoangeles2001@gmail.com>
2025-12-24 09:45:19 +0800

    cleanup

--- src/RouteData.jsx
+++ src/RouteData.jsx
@@ -1,14 +1,6 @@
 import Home from './pages/home';
 
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import LeaderboardOutlinedIcon from '@mui/icons-material/LeaderboardOutlined';
-import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
-import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
-import WorkspacePremiumOutlinedIcon from '@mui/icons-material/WorkspacePremiumOutlined';
-import PermMediaOutlinedIcon from '@mui/icons-material/PermMediaOutlined';
-import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
-import FormatAlignJustifyOutlinedIcon from '@mui/icons-material/FormatAlignJustifyOutlined';
-import CodeOutlinedIcon from '@mui/icons-material/CodeOutlined';
 export const RouteData = [
     {
         title: 'About',
